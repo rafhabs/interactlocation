1d8111db13e8412d41369a5ff84d2ffffb5d82e3
rafhabs <rafhabs@gmail.com>
2021-05-03 22:50:36 -0300

    location com configuracoes

---
+++ app/build/generated/source/buildConfig/debug/com/rafhabs/interactlocation/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.rafhabs.interactlocation;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.rafhabs.interactlocation";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
